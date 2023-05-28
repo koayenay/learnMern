1de35c9883aa535d1f131fa2f12325a717e683eb
koayenay <koayenay@gmail.com>
2023-05-28 11:43:38 -0700

    database connection added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const colors = require("colors")
+const connectDB = require("./config/db")
 const { errorHandler } = require("./middleware/errorMiddleware")
 
 dotenv.config()
 
 const port = process.env.PORT || 5000
+connectDB()
 
 const app = express()
 
