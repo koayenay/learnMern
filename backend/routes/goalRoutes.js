e2648d885c8778a07708e4cdca3031c4e218e324
koayenay <koayenay@gmail.com>
2023-05-31 10:05:00 -0700

    added userModel and updated goalModel

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -11,5 +11,3 @@ router.route("/").get(getGoals).post(setGoals)
 router.route("/:id").delete(deleteGoals).put(updateGoals)
 
 module.exports = router
-
-// https://youtu.be/-0exw-9YJBo?list=PLTMNWTDdd5z_CtvbzG6r1f4hjnCaphPTV&t=1516
